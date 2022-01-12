ebc60037eac9fb5489d0605c90243ebf339bbfe8
amuradyan <muradianator@gmail.com>
2022-01-12 14:57:26 +0400

    Implemented disc configs (#6)
    * [Feature] Redesigned the `Disk.Config`
    
    Previously, the API on `Disk.Config` provided a method for adding
    sections, that accepted `Probability`-s. It has been decided /for
    testing reasons/ to pass `Optional<Probability>`-s to it.
    
    Those are stored in a _dirty_ map of configs /which was previously
    thought to be the clean final one/.
    
    We did this since we have not thought of use cases for a clean config
    map yet. We'll work around them when we do.
    
    Also, fixed the gradle arguments for GitHub actions

--- src/main/java/shwxr7/model/Disc.java
+++ src/main/java/shwxr7/model/Disc.java
@@ -2,6 +2,7 @@ package shwxr7.model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Disc {
   private final Config config;
@@ -11,26 +12,39 @@ public class Disc {
   }
 
   public static final class Config {
-    private final Map<Element, Probability> elementProbabilities = new HashMap<>();
+    private final Map<Element, Optional<Probability>> dirtyElementProbabilities = new HashMap<>();
 
     public Config() {
     }
 
-    public Config withSegment(Element element, Probability probability) {
-      elementProbabilities.put(element, probability);
+    public Config withSection(Element element, Optional<Probability> probability) {
+      dirtyElementProbabilities.put(element, probability);
+
       return this;
     }
 
-    public Disc build() {
-      if (configIsValid()) {
-        return new Disc(this);
+    public Optional<Disc> build() {
+      if (configIsValid(dirtyElementProbabilities)) {
+        return Optional.of(new Disc(this));
       } else {
-        throw new IllegalArgumentException("Invalid configuration");
+        return Optional.empty();
       }
     }
 
-    private boolean configIsValid() {
-      return false;
+    protected boolean configIsValid(Map<Element, Optional<Probability>> elementProbabilities) {
+      return sumOfAllProbabilities(elementProbabilities) == 100 && !containsBadProbabilities(elementProbabilities);
+    }
+
+    protected boolean containsBadProbabilities(Map<Element, Optional<Probability>> elementProbabilities) {
+      return elementProbabilities.values().contains(Optional.empty());
+    }
+
+    protected Integer sumOfAllProbabilities(Map<Element, Optional<Probability>> elementProbabilities) {
+      var sumOfProbabilities = elementProbabilities.values().stream()
+          .map(p -> p.isPresent() ? p.get().probability : 0)
+          .reduce(0, (a, b) -> a + b);
+
+      return sumOfProbabilities;
     }
   }
 }
